3e5289b918b26c5f6ef47b48dbb4453dccc9fec4
coolesting <coolesting@gmail.com>
2011-11-08 19:58:39 -0800

    altering the scaffold function

--- lib/seimtra/scaffold.rb
+++ lib/seimtra/scaffold.rb
@@ -6,17 +6,54 @@ class Scaffold
 		@name 		= name
 		@argv 		= argv
 		@fields 	= fields
-		@with 		= with
-		@without	= without
+		@with 		= {}
+		@with_keys 	= ['search', 'pager']
+		@actions 	= ['table', 'rm', 'new', 'edit']
 
+		if with != nil
+			@with.each do |k,v|
+				@with[k] = v if @with_keys.include?(k)
+			end
+		end
+
+		if without != nil
+			without.each do |action|
+				@actions.delete(action)
+			end
+		end
+
+		@keys = @vals = []
 		argv.each do |item|
 			key, val = item.split(":")
 			@keys << key
 			@vals << val
 			@vars[val] = key 
 		end
+
+		@actions.uniq!
+
+		#implement the actions one by one
+		unless @actions.empty?
+			@actions.each do |action|
+				send("action_#{action}".to_sym)
+			end
+		end
 	end
 
 	def template_content(name)
 	end
+
+	private
+
+		def action_table
+		end
+
+		def action_rm
+		end
+
+		def action_new
+		end
+
+		def action_edit
+		end
 end
