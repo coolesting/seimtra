72a66489edb743ef044c25a7158517c4785dc2bd
coolesting <coolesting@gmail.com>
2011-11-15 18:29:11 -0800

    updating scaffold

--- lib/seimtra/scaffold.rb
+++ lib/seimtra/scaffold.rb
@@ -6,31 +6,56 @@ class Scaffold
 
 	def initialize(name, fields, argv, with, level)
 		@name 			= name
-		@argv 			= argv
 		@fields 		= fields
-		@functions 		= ['list', 'rm', 'new', 'edit', 'pager', 'search']
-		@route_contents = @template_contents = {}
-		@with 			= with != nil ? with : {}
+		@functions 		= @route_contents = @template_contents = @with = @t = {}
 		@level 			= level
 
-		@keys = @vals = []
-		argv.each do |item|
-			key, val = item.split(":")
-			@keys << key
-			@vals << val
-			@vars[val] = key 
+		@functions['admin'] = 'enable'
+		preprocess_data(with, argv)
+		#display in frontground
+		unless @functions['display'].empty?
 		end
-
-		#preprocess the functions
-		process_data
-		@functions.uniq!.each do |function|
-			send("process_#{function}") if self.respond_to?("process_#{function}", true)
+		
+		#background application
+		if @functions['admin'] == 'enable'
+# 		@admin_functions.uniq!.each do |function|
+# 			send("process_#{function}") if self.respond_to?("process_#{function}", true)
 		end
 	end
 
 	private
 
-		def process_data
+		def preprocess_data(with, argv)
+			@keys = @vals = []
+			argv.each do |item|
+				key, val = item.split(":")
+				@keys << key
+				@vals << val
+				@vars[val] = key 
+			end
+
+			display = {}
+			display['pager'] 	= ['page_size', 'pager', 'page', 'ps']
+			display['search'] 	= ['search_by', 'search', 'src']
+			display['rm'] 		= ['delete_by', 'delete', 'rm', 'remove', 'remove_by']
+			display['edit'] 	= ['update_by', 'up', 'update']
+			display['new'] 		= ['new', 'create']
+			display['admin']	= ['enable', 'disable']
+
+			display.each do |key, val|
+				val.each do |item|
+					if with[item]
+						@t[with[key][0]] = with[item] 
+						@functions[key] = 'enable' 
+						break
+					end
+				end
+			end
+
+			@t['insert_sql'] = ''
+			@t['update_sql'] = ''
+
+			@functions['admin'] = with['admin'] if with['admin']
 		end
 
 		def process_list
@@ -40,28 +65,24 @@ class Scaffold
 			@template_contents[@name] = get_erb_content('show')
 		end
 
-		def process_page
-			@page_size = @with['page'].to_i
+		def process_pager
 			@route_contents += "\n#== pager =================================\n"
 			@route_contents += get_erb_content('page', 'routes')
 			@template_contents[@name] += get_erb_content('page')
 		end
 
 		def process_rm
-			@delete_by = ''
 			@route_contents += "\n#== remove ================================\n"
 			@route_contents += get_erb_content('rm', 'routes')
 		end
 
 		def process_search
-			@search_by = @with['search']
 			@route_contents += "\n#== search ================================\n"
 			@route_contents += get_erb_content('search', 'routes')
 			@template_contents[@name] = get_erb_content('search') + @template_contents[@name]
 		end
 
 		def process_new
-			@insert_sql = ''
 			@route_contents += "\n#== new ===================================\n"
 			@route_contents += get_erb_content('new', 'routes')
 			@template_names << 'new'
@@ -69,8 +90,6 @@ class Scaffold
 		end
 
 		def process_edit
-			@update_by = ''
-			@update_sql = ''
 			@route_contents += "\n#== edit ==================================\n"
 			@route_contents += get_erb_content('edit', 'routes')
 			@template_names << 'edit'
