08540e52c8938dfc52aa7a311ec19bcca2ee846c
coolesting <coolesting@gmail.com>
2011-12-12 18:22:10 -0800

    combining the schema and migration method

--- lib/bin/projects/db.rb
+++ lib/bin/projects/db.rb
@@ -1,74 +1,15 @@
 class SeimtraThor < Thor
 
-	# = Schema
+	# = database operation 
 	#
+	# Create and implement for migrations to the database,
 	# output/dump the schema/migration from database
-
-	# == options
-	#
-	# --dump		rollback the migration record, as the parameter of sequel
-
-	# == examples
-	#
-	#	3s schema
-	#	3s schema -d
-	#
-	# run the schema at db/schema.rb
-	#
-	# 	3s schema -r
-
-	method_option :details, :type => :boolean, :aliases => '-d'
-	method_option :run, :type => :boolean, :aliases => '-r'
-	method_option :dump, :type => :boolean
-	method_option :with, :type => :string
-	desc "schema", "Initialize a database with a schema"
-	def schema
-		
-		db 		= Db.new
-		dbcont 	= "'#{settings.db_connect}'"
-		output	= true
-		return error(db.msg) if db.error
-
-		#dump the mrgration from database
-		if options[:dump]
-			dump 	= options[:with] == 'D' ? '-D' : '-d'
-			mpath = Dir.pwd + "/migrations"
-			unless File.exist?(mpath)
-				empty_directory mpath
-			end
-			run("sequel #{dump} #{dbcont} > #{Dir.pwd}/db/migrations/#{Time.now.strftime('%Y%m%d%H%M%S')}.rb")
-		end
-
-		if output
-			say "The adapter :  #{db.get_scheme}.", "\e[32m"
-			say "The schema as the following.", "\e[32m"
-			puts "\n"
-
-			#puts the tables of database to array of hash
-			unless options.details?
-				db.get_tables.each do |table|
-					print table.to_s.ljust(20, ' ')
-					print db.get_columns(table)
-					print "\n"
-				end
-			else
-				print db.dump_schema_migration
-			end
-		end
-		say "\n"
-
-	end
-
-
-	# = migration database record
-	#
-	# Create and implement for migrations to database
 	#
 
 	# == arguments
 	#
 	# operate_table, string, such as, create:books, alter:books/
-	# fields, 		 array, ["String:name","String:password"]	
+	# fields, 		 array, ["String:name","String:password"]
 	#
 
 	# == options
@@ -76,31 +17,46 @@ class SeimtraThor < Thor
 	# --autocomplete, -a completing the fileds with primary_key, and timestamp, 
 	# 				automatically
 	# --run, -r		run the migrations
-	# --schema, -s	run the schema
-	# --to,-t		specify a module for implementing the migrating action
+	# --to, -t		specify a module for implementing the migrating action
 	# --version, -v	specify a version for migrating record
+	# --dump, -d		dump the database schema to a migration file
+	# --output, -o	output the schema of database
+	# --with, -w	a hash as the parammeters
 
 	# == examples
 	#
-	#	3s m drop :table1,:table2,:table3
-	#	3s m rename :old_table,:new_table
+	# create the migration
 	#
-	#	3s m create:table_name primary_key:uid String:name String:pawd
-	#	3s m alter:table_name drop_column:column_name
-	#	3s m alter:table_name rename_column:old_column_name,:new_column_name
+	#	3s db drop :table1,:table2,:table3
+	#	3s db rename :old_table,:new_table
+	#
+	#	3s db create:table_name primary_key:uid String:name String:pawd
+	#	3s db alter:table_name drop_column:column_name
+	#	3s db alter:table_name rename_column:old_column_name,:new_column_name
 	#
 	# create and run the migration records
 	#
-	# 	3s m table String:title text:body -a -r
+	# 	3s db table String:title text:body -a -r
 	#
+	# dump the db schema to migration
+	#
+	#	3s db -d
+	#	3s db --dump --with=D
+	#
+	# run the schema at db/schema.rb
+	#
+	#	3s db -r -w=schema
+
 
 	method_option :autocomplete, :type => :boolean, :aliases => '-a'
 	method_option :run, :type => :boolean, :aliases => '-r' 
-	method_option :schema, :type => :boolean, :aliases => '-s' 
 	method_option :to, :type => :string, :aliases => '-t' 
 	method_option :version, :type => :numeric, :aliases => '-v' 
-	desc "migration [OPERATER]:[TABLE] [FIELDS]", "Create/Run the migrations record for the database"
-	def migration(operate_table = nil, *argv)
+	method_option :dump, :type => :boolean, :aliases => '-d'
+	method_option :output, :type => :boolean, :aliases => '-o'
+	method_option :with, :type => :hash, :default => {}, :aliases => '-w'
+	desc "db [OPERATER]:[TABLE] [FIELDS]", "Create/Run the migrations, and output schema/migration of database"
+	def db(operate_table = nil, *argv)
 
 		#initialize data
 		db 				= Db.new
@@ -109,15 +65,14 @@ class SeimtraThor < Thor
 		module_current	= options[:to] == nil ? SCFG.get("module_focus") : options[:to]
 		path 			= "/modules/#{module_current}/migrations"
 		mpath 			= Dir.pwd + path
+		gpath 			= Dir.pwd + "/db/migrations"
 		operate 		= table = ''
 		default_operate	= ['create', 'alter', 'drop', 'rename']
-
 		dbcont 			= "'#{settings.db_connect}'"
 		version			= options[:version] == nil ? '' : "-M #{options[:version]}"
 
-		unless File.directory?(mpath)
-			empty_directory mpath
-		end
+		empty_directory(gpath) unless File.exist?(gpath)
+		empty_directory(mpath) unless File.directory?(mpath)
 
  		if operate_table != nil
 			if operate_table.index(':')
@@ -161,20 +116,43 @@ class SeimtraThor < Thor
 			end
 		end
 
-		#setting the running environment
+		#implement the migrations
 		if options.run? 
-			return error "No migrattion record file, please check #{mpath}" unless File.exist?(mpath)
-			run("sequel -m #{mpath} #{version} #{dbcont}")
+			path = mpath
+			if options[:with]['global']
+				path = Dir[gpath + "/*"].sort.last
+			end
+
+			return error("No schema at #{path}") unless File.exist?(path)
+			run("sequel -m #{path} #{version} #{dbcont}")
+			say "Implementing complete", "\e[32m"
 		end
 
-		#implement schema 
-		if options.schema?
-			spath = Dir.pwd + "/db/schema.rb"
-			return error("No schema at #{spath}") unless File.exist?(spath)
-			require spath
+		#dump the database schema to a  mrgration
+		if options.dump?
+			dump = options[:with]['dump'] == 'D' ? '-D' : '-d'
+			run("sequel #{dump} #{dbcont} > #{gpath}/#{Time.now.strftime('%Y%m%d%H%M%S')}.rb")
 		end
 
-		say "Implementing complete", "\e[32m"
+		#output the schema/migration
+		if options.output?
+			say "The adapter :  #{db.get_scheme}.", "\e[32m"
+			say "The schema as the following.", "\e[32m"
+			puts "\n"
+
+			#puts the tables of database to array of hash
+			if options[:with]["details"]
+				print db.dump_schema_migration
+			else
+				db.get_tables.each do |table|
+					print table.to_s.ljust(20, ' ')
+					print db.get_columns(table)
+					print "\n"
+				end
+			end
+			say "\n"
+		end
+
 	end
 
 end
