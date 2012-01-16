e6e6a0cc7b80c423dadeaff462326312c242a2a7
coolesting <coolesting@gmail.com>
2012-01-15 23:28:49 -0800

    updating the db task, improving the functionality of creating migration file

--- lib/bin/projects/db.rb
+++ lib/bin/projects/db.rb
@@ -2,58 +2,67 @@ class SeimtraThor < Thor
 
 	# = Database operation 
 	#
-	# Create and implement for migrations to the database,
+	# Create and implement the migration to database,
 	# output/dump the schema/migration from database
 	#
 	# == Arguments
 	#
-	# operate_table, string, such as, create:books, alter:books/
-	# fields, 		 array, ["String:name","String:password"]
+	# operator, 	string, e.g. , create, alter, rename, drop
+	# argv, 		array, e.g. , ["table_name", "String:name","String:password"]
 	#
 	# == Options
 	#
-	# --autocomplete, -a completing the fileds with primary_key, and timestamp, 
-	# 				automatically
+	# --autocomplete, -a completing the fileds with primary_key, and timestamp, automatically
 	# --run, -r		run the migrations
 	# --to, -t		specify a module for implementing the migrating action
 	# --version, -v	specify a version for migrating record
-	# --dump, -d		dump the database schema to a migration file
+	# --dump, -d	dump the database schema to a migration file
 	# --output, -o	output the schema of database
-	# --with, -w	a hash as the parammeters
+	# --details 	output with the details
+	# --schema		implement a global database schema
 	#
 	# == Examples
 	#
-	# create the migration
+	# dropdown/rename the table
 	#
 	#	3s db drop :table1,:table2,:table3
 	#	3s db rename :old_table,:new_table
 	#
-	#	3s db create:table_name primary_key:uid String:name String:pawd
-	#	3s db alter:table_name drop_column:column_name
-	#	3s db alter:table_name rename_column:old_column_name,:new_column_name
+	# create/alter the table,
 	#
-	# create and run the migration records
+	#	3s db create table_name primary_key:uid String:name String:pawd
 	#
-	# 	3s db table String:title text:body -a -r
+	#	3s db alter table_name drop_column:column_name
+	#	3s db alter table_name rename_column:old_column_name,:new_column_name
 	#
-	# dump the db schema to migration
+	# create and auto complete the fileds, primary id, created time,  
+	# then run the migration records
 	#
-	#	3s db -d
-	#	3s db --dump --with=D
+	# 	3s db create table_name String:title text:body -a -r
 	#
-	# run the schema at db/schema.rb
+	# dump the current db schema to a migration file (the default path as db/migrations)
 	#
-	#	3s db -r -w=schema
+	#	3s db --dump=D
+	#
+	# implement a db schema using the default file at db/migrations
+	#
+	#	3s db -r --schema
+	#
+	# output the schema of current database
+	#
+	#	3s db -o
+	#	3s db -o --details
 
 	method_option :autocomplete, :type => :boolean, :aliases => '-a'
 	method_option :run, :type => :boolean, :aliases => '-r' 
 	method_option :to, :type => :string, :aliases => '-t' 
 	method_option :version, :type => :numeric, :aliases => '-v' 
-	method_option :dump, :type => :boolean, :aliases => '-d'
+	method_option :dump, :type => :string
 	method_option :output, :type => :boolean, :aliases => '-o'
-	method_option :with, :type => :hash, :default => {}, :aliases => '-w'
-	desc "db [OPERATOR:TABLE] [FIELDS]", "Create/Run the migrations, and output schema/migration of database"
-	def db(operate_table = nil, *argv)
+	method_option :details, :type => :boolean
+	method_option :schema, :type => :boolean
+	desc "db [OPERATOR] [ARGV]", "Create/Run the migrations, and output schema/migration of database"
+	def db(operator = nil, *argv)
 
 		#initialize data
 		db 				= Db.new
@@ -63,44 +72,43 @@ class SeimtraThor < Thor
 		path 			= "/modules/#{module_current}/migrations"
 		mpath 			= Dir.pwd + path
 		gpath 			= Dir.pwd + "/db/migrations"
-		operate 		= table = ''
-		default_operate	= [:create, :alter, :drop, :rename]
+		doperator		= [:create, :alter, :drop, :rename]
 		dbcont 			= "'#{settings.db_connect}'"
 		version			= options[:version] == nil ? '' : "-M #{options[:version]}"
 
 		empty_directory(gpath) unless File.exist?(gpath)
 		empty_directory(mpath) unless File.directory?(mpath)
 
- 		if operate_table != nil
-			if operate_table.index(':')
-				operate, table = operate_table.split(":")
-			else
-				operate = 'create'
-				table	= operate_table
+		#create a migration record
+ 		if operator != nil and argv.count > 0
+
+			unless doperator.include? operator.to_sym
+				error("The #{operator} is a error operator, you allow to use create, \
+				alter, rename and drop as the operator")
 			end
 
-			error("#{operate} is a error operation, you allow to use create, \
-			alter, rename and drop") unless default_operate.include?(operate.to_sym)
-		end
+			if [:create, :alter].include? operator.to_sym
+				table = argv.shift
+			else
+				table = ''
+			end
 
-		#create file for migrating record
-		if operate != '' and table != '' and argv.count > 0
-			file = mpath + "/#{Time.now.strftime("%Y%m%d%H%M%S")}_#{operate}_#{table}.rb"
+			file = mpath + "/#{Time.now.strftime("%Y%m%d%H%M%S")}_#{operator}_#{table}.rb"
 
 			#auto add the primary_key and time to migrating record
 			if options.autocomplete?
 				error db.msg if db.error
-				argv = db.autocomplete(table, argv) if operate == 'create'
+				argv = db.autocomplete(table, argv) if operator == 'create'
 			end
 	
 			create_file file do
 				content = "Sequel.migration do\n"
 				content << "\tchange do\n"
 
-				if operate == "drop" or operate == "rename"
-					content << "\t\t#{operate}_table(#{argv.to_s.gsub(",", ", ")})\n"
+				if operator == "drop" or operator == "rename"
+					content << "\t\t#{operator}_table(#{argv.to_s.gsub(",", ", ")})\n"
 				else
-					content << "\t\t#{operate}_table(:#{table}) do\n"
+					content << "\t\t#{operator}_table(:#{table}) do\n"
 					argv.each do |item|
 						content << "\t\t\t#{item.sub(":", " :").gsub(",", ", ")}\n"
 					end
@@ -115,27 +123,27 @@ class SeimtraThor < Thor
 		#implement the migrations
 		if options.run? 
 			path = mpath
-			path = Dir[gpath + "/*"].sort.last if options[:with]['global']
+			path = Dir[gpath + "/*"].sort.last if options.schema?
 
 			error("No schema at #{path}") unless File.exist?(path)
 			run("sequel -m #{path} #{version} #{dbcont}")
 			isay "Implementing complete"
 		end
 
-		#dump the database schema to a  mrgration
-		if options.dump?
-			dump = options[:with]['dump'] == 'D' ? '-D' : '-d'
+		#dump the database schema to a mrgration file
+		if options[:dump] != nil
+			dump = options[:dump] == 'D' ? '-D' : '-d'
 			run("sequel #{dump} #{dbcont} > #{gpath}/#{Time.now.strftime('%Y%m%d%H%M%S')}.rb")
 		end
 
 		#output the schema/migration
 		if options.output?
-			isay "The adapter :  #{db.get_scheme}."
+			isay "The adapter is :  #{db.get_scheme}."
 			isay "The schema as the following."
 			puts "\n"
 
 			#puts the tables of database to array of hash
-			if options[:with]["details"]
+			if options.details?
 				print db.dump_schema_migration
 			else
 				db.get_tables.each do |table|
