1f8ebfa7ba467ca78d1a5ed8d5ae5ab49c5606f1
coolesting <coolesting@gmail.com>
2012-06-13 20:34:53 -0700

    update the generate task, supply the new features, allow one to generate the route, view, form

--- lib/bin/projects/generate.rb
+++ lib/bin/projects/generate.rb
@@ -14,11 +14,16 @@ class SeimtraThor < Thor
 	#
 	# == Examples 
 	#
-	# 3s g table_name field1 field2 field3
+	# create a scaffold
+	#
+	#	3s g table_name field1 field2 field3 -rvf
 	#
 
 	desc "generate [TABLE_NAME] [FIELDS]", "Generate a scaffold for module"
 	method_option :to, :type => :string, :aliases => '-t'
+	method_option :route, :type => :boolean, :aliases => '-r'
+	method_option :view, :type => :boolean, :aliases => '-v'
+	method_option :form, :type => :boolean, :aliases => '-f'
 	map 'g' => :generate
 	def generate *argv
 
@@ -26,13 +31,14 @@ class SeimtraThor < Thor
 			module_name = options[:to] ? options[:to] : get_module
 			file_name	= argv.shift
 
-			files 		= {
-				"tmp/route.app"	=> "modules/#{module_name}/#{Sbase::Folders[:app]}/#{file_name}.rb",
-				"tmp/view.tpl"	=> "modules/#{module_name}/#{Sbase::Folders[:tpl]}/#{file_name}.slim",
-				"tmp/form.tpl"	=> "modules/#{module_name}/#{Sbase::Folders[:tpl]}/#{file_name}_form.slim"
-			}
+			files 				= {}
+			files["route.app"] 	= "#{Sbase::Folders[:app]}/#{file_name}.rb" if options.route?
+			files["view.tpl"] 	= "#{Sbase::Folders[:tpl]}/#{module_name}_#{file_name}.slim" if options.view?
+			files["form.tpl"] 	= "#{Sbase::Folders[:tpl]}/#{module_name}_#{file_name}_form.slim" if options.form?
 
 			files.each do | source, target |
+				source = "tmp/#{source}"
+				target = "modules/#{module_name}/#{target}"
 				template(source, target) unless File.exist?(target)
 			end
  		else
